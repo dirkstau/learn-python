1a9f2ac0a7e36fce66a7690439a04901572f9345
dirkstau <dirkstau@googlemail.com>
2020-04-02 13:38:22 +0200

    finale Version

--- 200401_Schachbrett_Reiskörner.py
+++ 200401_Schachbrett_Reiskörner.py
@@ -1,18 +1,12 @@
 """Dieses Script berechnet die berühmte Reiskornlegende von Sissa ibn Dahir."""
 
-import matplotlib.pyplot as plt
 summe = 0
-feldListe = []
 
 for feld in range(64):
     reiskorn = 2**feld
-    feldListe.append(reiskorn)
     summe += reiskorn
     print(f"Feld {feld+1:>2}: {reiskorn:>25,} Reiskörner und damit insgesamt \
           {summe:>26,} Reiskörner")
 
 gewicht = summe * 0.02 / 1000000
 print(f"Das Gesamtgewicht beträgt etwa {gewicht:,.0f} t")
-
-plt.plot(feldListe)
-plt.show()
